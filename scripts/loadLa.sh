76c27a367af5d01bf14be51e963c8107bcb3a543
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-04-29 17:26:03 +0100

    Specify LATIN1 char encoding for shapefiles
    Otherwise shp2pgsql fails with encoding errors
    (for me)

--- scripts/loadLa.sh
+++ scripts/loadLa.sh
@@ -5,7 +5,7 @@ wget 'https://data.gov.uk/dataset/county-and-unitary-authorities-ew-2012-boundar
 mkdir /tmp/la
 unzip /tmp/la.zip -d /tmp/la
 unzip '/tmp/la/data/County_and_unitary_authorities_(E+W)_2012_Boundaries_(Full_Extent).zip' -d /tmp/la/data
-shp2pgsql /tmp/la/data/CTYUA_DEC_2012_EW_BFE.shp la | psql -d tombolo
+shp2pgsql -W LATIN1 /tmp/la/data/CTYUA_DEC_2012_EW_BFE.shp la | psql -d tombolo
 psql -d tombolo -c "insert into geography_object(geography_type_label, label, name, shape) select 'localAuthority', ctyua12cd, ctyua12nm, ST_Transform(ST_SETSRID(geom, 27700),4326) from la"
 psql -d tombolo -c 'drop table la'
 rm -r /tmp/la
--- scripts/loadLsoa.sh
+++ scripts/loadLsoa.sh
@@ -3,7 +3,7 @@ set -e -x
 wget 'https://geoportal.statistics.gov.uk/Docs/Boundaries/Lower_layer_super_output_areas_(E+W)_2011_Boundaries_(Generalised_Clipped)_V2.zip' -O /tmp/lsoa.zip
 mkdir /tmp/lsoa
 unzip /tmp/lsoa.zip -d /tmp/lsoa
-shp2pgsql /tmp/lsoa/LSOA_2011_EW_BGC_V2.shp lsoa | psql -d tombolo
+shp2pgsql -W LATIN1 /tmp/lsoa/LSOA_2011_EW_BGC_V2.shp lsoa | psql -d tombolo
 psql -d tombolo -c "insert into geography_object(geography_type_label, label, name, shape) select 'lsoa', lsoa11cd, lsoa11nm, ST_Transform(ST_SETSRID(geom, 27700),4326) from lsoa"
 psql -d tombolo -c 'drop table lsoa'
 rm -r /tmp/lsoa
