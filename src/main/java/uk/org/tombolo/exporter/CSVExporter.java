317f189c83feb82e31389d53cb439b897647b78f
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-05-04 15:40:21 +0100

    Add basic attributes to CSVExporter
    We can't keep working with lists when we have
    nested data structures. Committing now because the
    next step is to reorganise tabulateGeographies
    to use a map which can then be tabulated later
    when we know all the keys that will be inside.

--- src/main/java/uk/org/tombolo/exporter/CSVExporter.java
+++ src/main/java/uk/org/tombolo/exporter/CSVExporter.java
@@ -5,37 +5,62 @@ import java.util.*;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import uk.org.tombolo.core.Attribute;
 import uk.org.tombolo.core.Geography;
 import uk.org.tombolo.core.GeographyType;
+import uk.org.tombolo.core.Provider;
+import uk.org.tombolo.core.utils.AttributeUtils;
 import uk.org.tombolo.core.utils.GeographyTypeUtils;
 import uk.org.tombolo.core.utils.GeographyUtils;
+import uk.org.tombolo.core.utils.ProviderUtils;
+import uk.org.tombolo.execution.spec.AttributeSpecification;
 import uk.org.tombolo.execution.spec.DatasetSpecification;
 import uk.org.tombolo.execution.spec.GeographySpecification;
+
+import javax.json.JsonValue;
 
 public class CSVExporter implements Exporter {
 
 	@Override
 	public void write(Writer writer, DatasetSpecification datasetSpecification) throws Exception {
 		CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader());
-		printer.printRecord(Arrays.asList("type_label", "label", "name", "geometry"));
+		printer.printRecord(Arrays.asList("label", "name", "geometry", "attr_name", "attr_provider"));
 		printer.printRecords(
-				tabulateGeographies(getGeographies(datasetSpecification))
+				tabulateGeographies(getGeographies(datasetSpecification), datasetSpecification)
 		);
 	}
 
-	public List<List<Object>> tabulateGeographies(List<Geography> geographies) {
+	public List<List<Object>> tabulateGeographies(List<Geography> geographies, DatasetSpecification datasetSpecification) {
 		List<List<Object>> table = new ArrayList<List<Object>>();
+
 		for(Geography geography : geographies) {
 			List<Object> row = new ArrayList<Object>();
-			row.add(geography.getGeographyType().getLabel());
+
 			row.add(geography.getLabel());
 			row.add(geography.getName());
 			row.add(geography.getShape().toString());
+
+			List<AttributeSpecification> attributeSpecs = datasetSpecification.getAttributeSpecification();
+			for (AttributeSpecification attributeSpec : attributeSpecs) {
+				Provider provider = ProviderUtils.getByLabel(attributeSpec.getProviderLabel());
+				Attribute attribute = AttributeUtils.getByProviderAndLabel(provider, attributeSpec.getAttributeLabel());
+
+				row.addAll(getAttributeProperty(geography, attribute, attributeSpec));
+			}
 
 			table.add(row);
 		}
 
 		return table;
+	}
+
+	private List<Object> getAttributeProperty(Geography geography, Attribute attribute, AttributeSpecification attributeSpec) {
+		List<Object> property = new ArrayList<Object>();
+
+		property.add(attribute.getName());
+		property.add(attribute.getProvider().getName());
+
+		return property;
 	}
 
 	private List<Geography> getGeographies(DatasetSpecification datasetSpecification) {
