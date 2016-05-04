723f6102a555103750c96d410e871ed6bfcad093
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-05-04 14:32:34 +0100

    Impl CSVExporter for basic data
    That's just type, label, geometry etc. I need to
    figure out what's meant to be output here and do
    that.
    
    The types in here can probably be improved.

--- src/main/java/uk/org/tombolo/exporter/CSVExporter.java
+++ src/main/java/uk/org/tombolo/exporter/CSVExporter.java
@@ -1,14 +1,54 @@
 package uk.org.tombolo.exporter;
 
 import java.io.Writer;
+import java.util.*;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import uk.org.tombolo.core.Geography;
+import uk.org.tombolo.core.GeographyType;
+import uk.org.tombolo.core.utils.GeographyTypeUtils;
+import uk.org.tombolo.core.utils.GeographyUtils;
 import uk.org.tombolo.execution.spec.DatasetSpecification;
+import uk.org.tombolo.execution.spec.GeographySpecification;
 
 public class CSVExporter implements Exporter {
 
 	@Override
 	public void write(Writer writer, DatasetSpecification datasetSpecification) throws Exception {
-		writer.write("mate we got some java");
+		CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader());
+		printer.printRecord(Arrays.asList("type_label", "label", "name", "geometry"));
+		printer.printRecords(
+				tabulateGeographies(getGeographies(datasetSpecification))
+		);
+	}
+
+	public List<List<Object>> tabulateGeographies(List<Geography> geographies) {
+		List<List<Object>> table = new ArrayList<List<Object>>();
+		for(Geography geography : geographies) {
+			List<Object> row = new ArrayList<Object>();
+			row.add(geography.getGeographyType().getLabel());
+			row.add(geography.getLabel());
+			row.add(geography.getName());
+			row.add(geography.getShape().toString());
+
+			table.add(row);
+		}
+
+		return table;
+	}
+
+	private List<Geography> getGeographies(DatasetSpecification datasetSpecification) {
+		List<Geography> geographies = new ArrayList<Geography>();
+
+		for(GeographySpecification geographySpecification : datasetSpecification.getGeographySpecification()){
+			GeographyType geographyType = GeographyTypeUtils.getGeographyTypeByLabel(geographySpecification.getGeographyType());
+			List<Geography> geographyList = GeographyUtils
+					.getGeographyByTypeAndLabelPattern(geographyType, geographySpecification.getLabelPattern());
+			geographies.addAll(geographyList);
+		}
+
+		return geographies;
 	}
 
 }
--- src/test/java/uk/org/tombolo/exporter/CSVExporterTest.java
+++ src/test/java/uk/org/tombolo/exporter/CSVExporterTest.java
@@ -1,13 +1,23 @@
 package uk.org.tombolo.exporter;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.junit.Test;
+import uk.org.tombolo.core.Geography;
+import uk.org.tombolo.core.GeographyType;
+import uk.org.tombolo.execution.spec.AttributeSpecification;
 import uk.org.tombolo.execution.spec.DatasetSpecification;
 import uk.org.tombolo.execution.spec.GeographySpecification;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -15,16 +25,47 @@ public class CSVExporterTest {
 	CSVExporter exporter = new CSVExporter();
 
 	@Test
-	public void testWrite() throws Exception {
+	public void testWrite() throws Exception{
 		Writer writer = new StringWriter();
-		DatasetSpecification spec = new DatasetSpecification();
-		List<GeographySpecification> geographySpecification = new ArrayList<GeographySpecification>();
-		geographySpecification.add(new GeographySpecification("localAuthority","E09%"));
-		spec.setGeographySpecification(geographySpecification);
 
-		exporter.write(writer, spec);
+		exporter.write(writer, makeDatasetSpecification());
 		writer.flush();
 
-		assertEquals("mate we got some java", writer.toString());
+		assertEquals("E09000033", getFirstFeatureLabel(writer.toString()));
+	}
+
+	@Test
+	public void tabulateGeographies() throws Exception {
+		List<Geography> geographies = new ArrayList<Geography>();
+		geographies.add(new Geography(
+				new GeographyType("type_label", "type_name"),
+				"label",
+				"name",
+				new GeometryFactory().createPoint(new Coordinate(0,0))
+		));
+		List<Object> geography = exporter.tabulateGeographies(geographies).get(0);
+
+		assertEquals("type_label",  (String) geography.get(0));
+		assertEquals("label",       (String) geography.get(1));
+		assertEquals("name",        (String) geography.get(2));
+		assertEquals("POINT (0 0)", (String) geography.get(3));
+
+	}
+
+	private DatasetSpecification makeDatasetSpecification() {
+		DatasetSpecification spec = new DatasetSpecification();
+		List<GeographySpecification> geographySpecification = new ArrayList<GeographySpecification>();
+		geographySpecification.add(new GeographySpecification("E09%", "localAuthority"));
+		List<AttributeSpecification> attributeSpecification = new ArrayList<AttributeSpecification>();
+		attributeSpecification.add(new AttributeSpecification("uk.gov.london", "populationDensity"));
+		spec.setGeographySpecification(geographySpecification);
+		spec.setAttributeSpecification(attributeSpecification);
+		return spec;
+	}
+
+	private String getFirstFeatureLabel(String csvString) throws IOException {
+		CSVParser parser = CSVParser.parse(csvString, CSVFormat.DEFAULT.withHeader());
+		List<CSVRecord> records = parser.getRecords();
+		return records.get(0).get("label");
 	}
 }
