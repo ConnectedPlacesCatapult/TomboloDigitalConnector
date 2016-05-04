c11d5fc4909858941aea7f3652b1c15be6ec527d
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-05-04 12:02:55 +0100

    Fix GeoJSONExporterTest
    Turns out that the GeographySpecification params
    were flipped, meaning the exporter returned an
    empty set. I thought the empty set was expected by
    the test but I don't think this any more.
    
    Had to add an AttributeSpecification to get it to
    run and then we got some real results out of it!
    
    Asserting against those real results is very
    unwieldy and should be mocked, as well as using a
    nicer JSON library, but we'll do that in a future
    commit. For now we just check against the label
    of the first feature.
    
    This should give us enough structure to make our
    CSV exporter testable.

--- src/test/java/uk/org/tombolo/exporter/GeoJsonExporterTest.java
+++ src/test/java/uk/org/tombolo/exporter/GeoJsonExporterTest.java
@@ -3,15 +3,22 @@ package uk.org.tombolo.exporter;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Test;
 
+import uk.org.tombolo.execution.spec.AttributeSpecification;
 import uk.org.tombolo.execution.spec.DatasetSpecification;
 import uk.org.tombolo.execution.spec.GeographySpecification;
+
 
 public class GeoJsonExporterTest {
 	GeoJsonExporter exporter = new GeoJsonExporter();
@@ -21,13 +28,24 @@ public class GeoJsonExporterTest {
 		Writer writer = new StringWriter();
 		DatasetSpecification spec = new DatasetSpecification();
 		List<GeographySpecification> geographySpecification = new ArrayList<GeographySpecification>();
-		geographySpecification.add(new GeographySpecification("localAuthority","E09%"));
+		geographySpecification.add(new GeographySpecification("E09%", "localAuthority"));
+		List<AttributeSpecification> attributeSpecification = new ArrayList<AttributeSpecification>();
+		attributeSpecification.add(new AttributeSpecification("uk.gov.london", "populationDensity"));
 		spec.setGeographySpecification(geographySpecification);
+		spec.setAttributeSpecification(attributeSpecification);
 		
 		exporter.write(writer, spec);
 		writer.flush();
-		
-		assertEquals("{\"type\":\"FeatureCollection\",\"features\":[]}", writer.toString());
+
+		assertEquals("E09000033", getFirstFeatureLabel(writer.toString()));
 	}
-	
+
+	private String getFirstFeatureLabel(String jsonString) throws ParseException {
+		JSONParser parser = new JSONParser();
+		JSONObject root = (JSONObject) parser.parse(jsonString);
+		JSONArray features = (JSONArray) root.get("features");
+		JSONObject firstFeature = (JSONObject) features.get(0);
+		JSONObject firstFeatureProperties = (JSONObject) firstFeature.get("properties");
+		return firstFeatureProperties.get("label").toString();
+	}
 }
