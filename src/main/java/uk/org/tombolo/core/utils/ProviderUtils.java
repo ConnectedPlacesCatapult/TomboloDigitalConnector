d506612aa209b7913ecd32078e5be44983284bed
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-05-05 14:46:21 +0100

    Move ProviderUtil session management into methods
    This should happen in all the *Utils classes, as
    otherwise we've got global mutable state which causes
    problems like e.g. Hibernate complains about duplicate
    objects showing up when they're fetched from different
    bits of the code.

--- src/main/java/uk/org/tombolo/core/utils/ProviderUtils.java
+++ src/main/java/uk/org/tombolo/core/utils/ProviderUtils.java
@@ -10,25 +10,37 @@ import org.hibernate.criterion.Restrictions;
 import uk.org.tombolo.core.Provider;
 
 public class ProviderUtils {
-
-	static Session session = HibernateUtil.getSessionFactory().openSession();
-	
 	public static Provider getTestProvider(){
-		return (Provider)session.load(Provider.class, "uk.org.tombolo.test");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			return (Provider)session.load(Provider.class, "uk.org.tombolo.test");
+		} finally {
+			session.close();
+		}
 	}
 	
 	public static void save(Provider provider){
-		session.beginTransaction();
-		// FIXME: This might be inefficient if we are updating the provider over and over again without actually changing it
-		session.saveOrUpdate(provider);
-		session.getTransaction().commit();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			session.beginTransaction();
+			// FIXME: This might be inefficient if we are updating the provider over and over again without actually changing it
+			session.saveOrUpdate(provider);
+			session.getTransaction().commit();
+		} finally {
+			session.close();
+		}
 	}
 
 	public static Provider getByLabel(String label){
-		Criteria criteria = session.createCriteria(Provider.class);
-		Map<String,Object> restrictions = new HashMap<String,Object>();
-		restrictions.put("label", label);
-		Provider provider = (Provider)criteria.add(Restrictions.allEq(restrictions)).uniqueResult();
-		return provider;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			Criteria criteria = session.createCriteria(Provider.class);
+			Map<String, Object> restrictions = new HashMap<String, Object>();
+			restrictions.put("label", label);
+			Provider provider = (Provider) criteria.add(Restrictions.allEq(restrictions)).uniqueResult();
+			return provider;
+		} finally {
+			session.close();
+		}
 	}
 }
