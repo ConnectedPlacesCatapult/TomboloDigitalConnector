29680182ef4cfcd088fa64fd93dd762e3063f7eb
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-05-10 11:55:10 +0100

    Add mocking to some missed LDS tests
    Plus fixtures

--- src/test/java/uk/org/tombolo/importer/londondatastore/LondonDatastoreImporterBaW.java
+++ src/test/java/uk/org/tombolo/importer/londondatastore/LondonDatastoreImporterBaW.java
@@ -1,18 +1,35 @@
 package uk.org.tombolo.importer.londondatastore;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import uk.org.tombolo.core.Attribute;
 import uk.org.tombolo.core.Datasource;
+import uk.org.tombolo.core.TimedValue;
 import uk.org.tombolo.core.utils.TimedValueUtils;
+import uk.org.tombolo.importer.AbstractImporterTestUtils;
+import uk.org.tombolo.importer.Importer;
+import uk.org.tombolo.importer.ons.ONSCensusImporter;
 
 public class LondonDatastoreImporterBaW {
 	private static final String DATASOURCE_ID = "walking-cycling-borough";
-	LondonDatastoreImporter importer = new LondonDatastoreImporter(new TimedValueUtils());
+	public Importer importer;
+	private TimedValueUtils mockTimedValueUtils;
+
+	@Before
+	public void before(){
+		mockTimedValueUtils = mock(TimedValueUtils.class);
+		when(mockTimedValueUtils.save(anyListOf(TimedValue.class))).thenAnswer(AbstractImporterTestUtils.listLengthAnswer);
+		importer = new LondonDatastoreImporter(mockTimedValueUtils);
+		AbstractImporterTestUtils.mockDownloadUtils(importer);
+	};
 	
 	@Test
 	public void testGetDatasource() throws Exception {
--- src/test/java/uk/org/tombolo/importer/londondatastore/LondonDatastoreImporterTestLbp.java
+++ src/test/java/uk/org/tombolo/importer/londondatastore/LondonDatastoreImporterTestLbp.java
@@ -1,15 +1,31 @@
 package uk.org.tombolo.importer.londondatastore;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import uk.org.tombolo.core.Datasource;
+import uk.org.tombolo.core.TimedValue;
 import uk.org.tombolo.core.utils.TimedValueUtils;
+import uk.org.tombolo.importer.AbstractImporterTestUtils;
+import uk.org.tombolo.importer.Importer;
+import uk.org.tombolo.importer.ons.ONSCensusImporter;
 
 public class LondonDatastoreImporterTestLbp {
+	public Importer importer;
+	private TimedValueUtils mockTimedValueUtils;
 
-	LondonDatastoreImporter importer = new LondonDatastoreImporter(new TimedValueUtils());
+	@Before
+	public void before(){
+		mockTimedValueUtils = mock(TimedValueUtils.class);
+		when(mockTimedValueUtils.save(anyListOf(TimedValue.class))).thenAnswer(AbstractImporterTestUtils.listLengthAnswer);
+		importer = new LondonDatastoreImporter(mockTimedValueUtils);
+		AbstractImporterTestUtils.mockDownloadUtils(importer);
+	};
 	
 	@Test
 	public void testGetDatasource() throws Exception {
