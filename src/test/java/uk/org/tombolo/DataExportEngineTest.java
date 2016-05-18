fc08bcc83c995f56e7485e19106c1dba8d6c5462
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-05-18 17:13:56 +0100

    Add JSONPath for handy JSON asserts

--- src/test/java/uk/org/tombolo/DataExportEngineTest.java
+++ src/test/java/uk/org/tombolo/DataExportEngineTest.java
@@ -1,23 +1,23 @@
 package uk.org.tombolo;
 
 import org.junit.Test;
-import uk.org.tombolo.execution.spec.DataExportSpecification;
 
 import java.io.StringWriter;
 import java.io.Writer;
 
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 public class DataExportEngineTest {
     DataExportEngine engine = new DataExportEngine();
     DataExportSpecificationBuilder builder = DataExportSpecificationBuilder.fromBlankGeoJson();
+    Writer writer = new StringWriter();
 
     @Test
     public void testReturnsEmptyOnBlankSpec() throws Exception {
-        Writer writer = new StringWriter();
-
         engine.execute(builder.build(), writer, true);
-
-        assertEquals("{\"type\":\"FeatureCollection\",\"features\":[]}", writer.toString());
+        
+        assertThat(writer.toString(), hasJsonPath("$.features", hasSize(0)));
     }
 }
