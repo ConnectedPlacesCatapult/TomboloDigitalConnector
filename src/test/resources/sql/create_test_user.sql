925ea7ba9325c214a325ef8cc7291bef90d39393
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-05-24 18:04:15 +0100

    Update README & test setup instructions

--- src/test/resources/sql/create_test_user.sql
+++ src/test/resources/sql/create_test_user.sql
@@ -1,5 +1,3 @@
 create role tombolo_test with password 'tombolo_test' LOGIN;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO tombolo_test;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO tombolo_test;
-
-CREATE EXTENSION postgis;
