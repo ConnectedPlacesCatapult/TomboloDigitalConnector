d0b18a2a70456c0a2ff09e45bc4463ab24ac6dd0
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-06-20 14:45:40 +0100

    Remove mocked field in GeoJsonExporterTest

--- src/test/java/uk/org/tombolo/exporter/GeoJsonExporterTest.java
+++ src/test/java/uk/org/tombolo/exporter/GeoJsonExporterTest.java
@@ -14,6 +14,7 @@ import uk.org.tombolo.core.Provider;
 import uk.org.tombolo.core.Subject;
 import uk.org.tombolo.core.utils.SubjectUtils;
 import uk.org.tombolo.field.FieldWithProvider;
+import uk.org.tombolo.field.FixedAnnotationField;
 import uk.org.tombolo.field.ValuesByTimeField;
 
 import java.io.StringWriter;
@@ -26,38 +27,6 @@ import static org.junit.Assert.assertEquals;
 
 public class GeoJsonExporterTest extends AbstractTest {
 	GeoJsonExporter exporter = new GeoJsonExporter();
-
-	private class FixedValueField implements FieldWithProvider {
-		private final String label;
-		private final Double value;
-
-		public FixedValueField(String label, Double value) {
-			this.label = label;
-			this.value = value;
-		}
-
-		@Override
-		public JSONObject jsonValueForSubject(Subject subject) {
-			JSONObject obj = new JSONObject();
-			obj.put(label, value);
-			return obj;
-		}
-
-		@Override
-		public String getLabel() {
-			return label;
-		}
-
-		@Override
-		public String getHumanReadableName() {
-			return label + "_readable";
-		}
-
-		@Override
-		public Provider getProvider() {
-			return new Provider("providerTestLabel", "providerTestName");
-		}
-	}
 
 	@Before
 	public void addSubjectFixtures() {
@@ -87,10 +56,10 @@ public class GeoJsonExporterTest extends AbstractTest {
 
 		exporter.write(writer,
 				Arrays.asList(SubjectUtils.getSubjectByLabel("E09000001")),
-				Arrays.asList(new FixedValueField("some_label", 100d))
+				Arrays.asList(new FixedAnnotationField("some_label", "some_value"))
 		);
 
-		assertEquals("100.0", JsonPath.read(writer.toString(), "$.features[0].properties.some_label").toString());
+		assertEquals("some_value", JsonPath.read(writer.toString(), "$.features[0].properties.some_label").toString());
 	}
 
 	private String getFirstFeatureLabel(String jsonString) throws ParseException {
