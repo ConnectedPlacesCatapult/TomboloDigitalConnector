e50e294d1bb1983ec76694f7e8940d18a9b6ae0e
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-07-01 12:08:59 +0100

    Refactor GeoJsonExporter to use org.json.simple
    Rather than string concatenation. This is a fair
    bit slower than the previous way. Investigating
    using a streaming writer instead for the next
    commit

--- src/main/java/uk/org/tombolo/exporter/GeoJsonExporter.java
+++ src/main/java/uk/org/tombolo/exporter/GeoJsonExporter.java
@@ -1,6 +1,7 @@
 package uk.org.tombolo.exporter;
 
 import org.geotools.geojson.geom.GeometryJSON;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,95 +14,45 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GeoJsonExporter implements Exporter {
 	private Logger log = LoggerFactory.getLogger(GeoJsonExporter.class);
 
-	// FIXME: Rewriter using geotools ... I could not get it to work quicly in the initial implementation (borkur)
-
 	public void write(Writer writer, List<Subject> subjects, List<Field> fields) throws IOException {
-		// Write beginning of subject list
-		writer.write("{");
-		writeStringProperty(writer, 0, "type", "FeatureCollection");
-		writeObjectPropertyOpening(writer, 1, "features",JsonValue.ValueType.ARRAY);
-		
-		int subjectCount = 0;
-		for (Subject subject : subjects){
-			// Subject is an a polygon or point for which data is to be output
+		JSONObject root = new JSONObject();
+		JSONArray features = new JSONArray();
+		root.put("type", "FeatureCollection");
+		root.put("features", features);
 
-			if (subjectCount > 0){
-				// This is not the first subject
-				writer.write(",\n");
+		root.put("features", subjects.stream()
+				.map(subject -> subjectToFeature(fields, subject))
+				.collect(Collectors.toList()));
+
+		writer.write(root.toJSONString());
+	}
+
+	private JSONObject subjectToFeature(List<Field> fields, Subject subject) {
+		// Subject is an a polygon or point for which data is to be output
+		JSONObject feature = new JSONObject();
+		JSONObject properties = new JSONObject();
+		feature.put("type", "Feature");
+		GeometryJSON geoJson = new GeometryJSON();
+		properties.put("geometry", geoJson.toString(subject.getShape()));
+		feature.put("properties", properties);
+
+		properties.put("label", subject.getLabel());
+		properties.put("name", subject.getName());
+
+		for (Field field : fields){
+			try {
+				properties.putAll(field.jsonValueForSubject(subject));
+			} catch (IncomputableFieldException e) {
+				log.warn("Could not compute Field %s for Subject %s, reason: %s", field.getLabel(), subject.getLabel(), e.getMessage());
+				properties.put(field.getLabel(), null);
 			}
-
-			// Open subject object
-			writer.write("{");
-			writeStringProperty(writer, 0, "type","Feature");
-
-			// Write geometry
-			GeometryJSON geoJson = new GeometryJSON();
-			StringWriter geoJsonWriter = new StringWriter();
-			geoJson.write(subject.getShape(),geoJsonWriter);
-			writer.write(", \"geometry\" : ");
-			geoJson.write(subject.getShape(), writer);
-
-
-			JSONObject properties = new JSONObject();
-			properties.put("label", subject.getLabel());
-			properties.put("name", subject.getName());
-
-			for (Field field : fields){
-				try {
-					properties.putAll(field.jsonValueForSubject(subject));
-				} catch (IncomputableFieldException e) {
-					log.warn("Could not compute Field %s for Subject %s, reason: %s", field.getLabel(), subject.getLabel(), e.getMessage());
-					properties.put(field.getLabel(), null);
-				}
-			}
-
-			writer.write(String.format(", \"properties\": %s", properties.toJSONString()));
-
-			// Close subject object
-			writer.write("}");
-
-			subjectCount++;
 		}
-		
-		// Write end of subject list
-		writer.write("]}");
-	}
 
-	protected void writeStringProperty(Writer writer, int propertyCount, String key, String value) throws IOException{
-		
-		if (propertyCount > 0)
-			writer.write(",");
-		
-		writer.write("\""+key+"\":\""+value+"\"");
-	}
-
-	protected void writeDoubleProperty(Writer writer, int propertyCount, String key, Double value) throws IOException{
-		
-		if (propertyCount > 0)
-			writer.write(",");
-		
-		writer.write("\""+key+"\":"+value+"");
-	}
-	
-	protected void writeObjectPropertyOpening(Writer writer, int propertyCount, String key, JsonValue.ValueType valueType) throws IOException{
-		if (propertyCount > 0)
-			writer.write(",");
-
-		writer.write("\""+key+"\":");
-		
-		switch(valueType){
-			case ARRAY:
-				writer.write("[");
-				break;
-			case OBJECT:
-				writer.write("{");
-				break;
-			default:
-				break;	
-		}
+		return feature;
 	}
 }
