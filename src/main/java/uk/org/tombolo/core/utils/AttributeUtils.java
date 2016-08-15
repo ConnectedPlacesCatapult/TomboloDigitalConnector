95d26f797d7db55f27ddaf6c3061c881fdea2c3b
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-08-15 12:06:51 +0100

    Refactor AttributeUtils to use HQL
    Since Criteria API is deprecated

--- src/main/java/uk/org/tombolo/core/utils/AttributeUtils.java
+++ src/main/java/uk/org/tombolo/core/utils/AttributeUtils.java
@@ -1,37 +1,19 @@
 package uk.org.tombolo.core.utils;
 
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.Query;
 import uk.org.tombolo.core.Attribute;
 import uk.org.tombolo.core.Provider;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class AttributeUtils {
-
-	public static Attribute getTestAttribute(){
-		return HibernateUtil.withSession(session -> {
-			Criteria criteria = session.createCriteria(Attribute.class);
-			Map<String, Object> restrictions = new HashMap<String, Object>();
-			restrictions.put("provider", ProviderUtils.getTestProvider());
-			restrictions.put("label", "testAttribute");
-			return (Attribute) criteria.add(Restrictions.allEq(restrictions)).uniqueResult();
-		});
-	}
-	
 	public static void save(List<Attribute> attributes){
 		HibernateUtil.withSession(session -> {
 			session.beginTransaction();
 			for (Attribute attribute : attributes) {
 				// FIXME: This might be inefficient if we are updating the attribute over and over again without actually changing it
-				Criteria criteria = session.createCriteria(Attribute.class);
-				Map<String, Object> restrictions = new HashMap<String, Object>();
-				restrictions.put("provider", attribute.getProvider());
-				restrictions.put("label", attribute.getLabel());
-				Attribute savedAttribute = (Attribute) criteria.add(Restrictions.allEq(restrictions)).uniqueResult();
+				Attribute savedAttribute = getByProviderAndLabel(attribute.getProvider(), attribute.getLabel());
 				if (savedAttribute == null) {
 					Integer id = (Integer) session.save(attribute);
 					attribute.setId(id);
@@ -53,24 +35,15 @@ public class AttributeUtils {
 	}
 
 	public static Attribute getByProviderAndLabel(Provider provider, String label){
-		return HibernateUtil.withSession(session -> {
-			Criteria criteria = session.createCriteria(Attribute.class);
-			Map<String, Object> restrictions = new HashMap<String, Object>();
-			restrictions.put("provider", provider);
-			restrictions.put("label", label);
-			Attribute attribute = (Attribute) criteria.add(Restrictions.allEq(restrictions)).uniqueResult();
-			return attribute;
-		});
+		return getByProviderAndLabel(provider.getLabel(), label);
 	}
 
 	public static Attribute getByProviderAndLabel(String providerLabel, String attributeLabel) {
 		return HibernateUtil.withSession(session -> {
-			Criteria criteria = session.createCriteria(Attribute.class);
-			Map<String, Object> restrictions = new HashMap<String, Object>();
-			restrictions.put("provider.label", providerLabel);
-			restrictions.put("label", attributeLabel);
-			Attribute attribute = (Attribute) criteria.add(Restrictions.allEq(restrictions)).uniqueResult();
-			return attribute;
+			Query query = session.createQuery("select a from Attribute a where provider.label = :providerLabel and label = :attributeLabel", Attribute.class);
+			query.setParameter("providerLabel", providerLabel);
+			query.setParameter("attributeLabel", attributeLabel);
+			return (Attribute) query.uniqueResult();
 		});
 	}
 	
