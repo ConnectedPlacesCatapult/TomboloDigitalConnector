03e09121882df300162096076e19105ecc289bbf
Borkur Sigurbjornsson <bsigurbjornsson@futurecities.catapult.org.uk>
2016-10-25 20:18:31 +0100

    Replaced the generic excel import of London Borough Walking and Cycling with an equivalent excel importer

---
+++ src/main/java/uk/org/tombolo/importer/londondatastore/WalkingCyclingBoroughImporter.java
@@ -0,0 +1,208 @@
+package uk.org.tombolo.importer.londondatastore;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.org.tombolo.core.Attribute;
+import uk.org.tombolo.core.Datasource;
+import uk.org.tombolo.core.TimedValue;
+import uk.org.tombolo.core.utils.TimedValueUtils;
+import uk.org.tombolo.importer.DownloadUtils;
+import uk.org.tombolo.importer.Importer;
+import uk.org.tombolo.importer.utils.ExcelUtils;
+import uk.org.tombolo.importer.utils.extraction.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Importer for importing the walking and cycling statistics for boroughs.
+ */
+public class WalkingCyclingBoroughImporter extends AbstractLondonDatastoreImporter implements Importer{
+    Logger log = LoggerFactory.getLogger(WalkingCyclingBoroughImporter.class);
+    public enum DatasourceId {walkingCyclingBorough};
+    public enum AttributeId {walk5xWeek,cycle1xWeek};
+
+    ExcelUtils excelUtils;
+
+    @Override
+    public void setDownloadUtils(DownloadUtils downloadUtils) {
+        super.setDownloadUtils(downloadUtils);
+        excelUtils = new ExcelUtils(downloadUtils);
+    }
+
+    @Override
+    public List<Datasource> getAllDatasources() throws Exception {
+        List<Datasource> datasources = new ArrayList<>();
+        for(DatasourceId datasourceId : DatasourceId.values()){
+            datasources.add(getDatasource(datasourceId.name()));
+        }
+        return datasources;
+    }
+
+    @Override
+    public Datasource getDatasource(String datasourceIdString) throws Exception {
+        DatasourceId datasourceId = DatasourceId.valueOf(datasourceIdString);
+        switch (datasourceId){
+            case walkingCyclingBorough:
+                Datasource datasource = new Datasource(
+                        DatasourceId.walkingCyclingBorough.name(),
+                        getProvider(),
+                        "Walking and Cycling in London Boroughs",
+                        "Walking and Cycling in London Boroughs"
+                );
+
+                datasource.setUrl("http://data.london.gov.uk/dataset/walking-and-cycling-borough");
+                datasource.setRemoteDatafile("https://files.datapress.com/london/dataset/walking-and-cycling-borough/walking-cycling-borough.xls");
+                datasource.setLocalDatafile("/LondonDatastore/walking-and-cycling-borough.xls");
+
+                for (AttributeId attributeId : AttributeId.values()) {
+                    datasource.addAttribute(getAttribute(attributeId));
+                }
+
+                return datasource;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    protected int importDatasource(Datasource datasource) throws Exception {
+        saveProviderAndAttributes(datasource);
+
+        Workbook workbook = excelUtils.getWorkbook(datasource);
+        RowCellExtractor subjectLabelExtractor = new RowCellExtractor(0, Cell.CELL_TYPE_STRING);
+        List<TimedValue> timedValueBuffer = new ArrayList<>();
+
+        // Extract walking
+        ConstantExtractor walk5xWeekAttributeLabelExtractor = new ConstantExtractor(AttributeId.walk5xWeek.name());
+        List<TimedValueExtractor> walk5xWeekExtractors = new ArrayList<>();
+        walk5xWeekExtractors.add(new TimedValueExtractor(
+                getProvider(),
+                subjectLabelExtractor,
+                walk5xWeekAttributeLabelExtractor,
+                new ConstantExtractor("2011-12-31T23:59:59"),
+                new RowCellExtractor(7,Cell.CELL_TYPE_NUMERIC)
+        ));
+        walk5xWeekExtractors.add(new TimedValueExtractor(
+                getProvider(),
+                subjectLabelExtractor,
+                walk5xWeekAttributeLabelExtractor,
+                new ConstantExtractor("2012-12-31T23:59:59"),
+                new RowCellExtractor(18, Cell.CELL_TYPE_NUMERIC)
+        ));
+        walk5xWeekExtractors.add(new TimedValueExtractor(
+                getProvider(),
+                subjectLabelExtractor,
+                walk5xWeekAttributeLabelExtractor,
+                new ConstantExtractor("2013-12-31T23:59:59"),
+                new RowCellExtractor(29, Cell.CELL_TYPE_NUMERIC)
+        ));
+        walk5xWeekExtractors.add(new TimedValueExtractor(
+                getProvider(),
+                subjectLabelExtractor,
+                walk5xWeekAttributeLabelExtractor,
+                new ConstantExtractor("2014-12-31T23:59:59"),
+                new RowCellExtractor(40, Cell.CELL_TYPE_NUMERIC)
+        ));
+        Iterator<Row> walkingIterator = workbook.getSheetAt(1).rowIterator();
+        walkingIterator.next();
+        walkingIterator.next();
+        walkingIterator.next();
+        walkingIterator.next();
+        while(walkingIterator.hasNext()){
+            Row row = walkingIterator.next();
+            subjectLabelExtractor.setRow(row);
+            for (TimedValueExtractor extractor : walk5xWeekExtractors){
+                ((RowCellExtractor)extractor.getValueExtractor()).setRow(row);
+                try {
+                    timedValueBuffer.add(extractor.extract());
+                }catch (UnknownSubjectLabelException e){
+                    // No worries if the subject does not exist
+                }catch (ExtractorException e){
+                    log.warn(e.getMessage());
+                }
+            }
+        }
+
+        // Extract cycling
+        ConstantExtractor cycle1xWeekAttributeLabelExtractor = new ConstantExtractor(AttributeId.cycle1xWeek.name());
+        List<TimedValueExtractor> cycle1xWeekExtractors = new ArrayList<>();
+        cycle1xWeekExtractors.add(new TimedValueExtractor(
+                getProvider(),
+                subjectLabelExtractor,
+                cycle1xWeekAttributeLabelExtractor,
+                new ConstantExtractor("2011-12-31T23:59:59"),
+                new RowCellExtractor(5,Cell.CELL_TYPE_NUMERIC)
+        ));
+        cycle1xWeekExtractors.add(new TimedValueExtractor(
+                getProvider(),
+                subjectLabelExtractor,
+                cycle1xWeekAttributeLabelExtractor,
+                new ConstantExtractor("2012-12-31T23:59:59"),
+                new RowCellExtractor(16, Cell.CELL_TYPE_NUMERIC)
+        ));
+        cycle1xWeekExtractors.add(new TimedValueExtractor(
+                getProvider(),
+                subjectLabelExtractor,
+                cycle1xWeekAttributeLabelExtractor,
+                new ConstantExtractor("2013-12-31T23:59:59"),
+                new RowCellExtractor(27, Cell.CELL_TYPE_NUMERIC)
+        ));
+        cycle1xWeekExtractors.add(new TimedValueExtractor(
+                getProvider(),
+                subjectLabelExtractor,
+                cycle1xWeekAttributeLabelExtractor,
+                new ConstantExtractor("2014-12-31T23:59:59"),
+                new RowCellExtractor(38, Cell.CELL_TYPE_NUMERIC)
+        ));
+        Iterator<Row> cyclingIterator = workbook.getSheetAt(2).rowIterator();
+        cyclingIterator.next();
+        cyclingIterator.next();
+        cyclingIterator.next();
+        cyclingIterator.next();
+        while(cyclingIterator.hasNext()){
+            Row row = cyclingIterator.next();
+            subjectLabelExtractor.setRow(row);
+            for (TimedValueExtractor extractor : cycle1xWeekExtractors){
+                ((RowCellExtractor)extractor.getValueExtractor()).setRow(row);
+                try {
+                    timedValueBuffer.add(extractor.extract());
+                }catch (UnknownSubjectLabelException e){
+                    // No worries if the subject does not exist
+                }catch (ExtractorException e){
+                    log.warn(e.getMessage());
+                }
+            }
+        }
+
+        TimedValueUtils.save(timedValueBuffer);
+        return timedValueBuffer.size();
+    }
+
+    private Attribute getAttribute(AttributeId attributeId){
+        switch (attributeId){
+            case walk5xWeek:
+               return new Attribute(
+                       getProvider(),
+                       AttributeId.walk5xWeek.name(),
+                       "Walk 5x Week",
+                        "% of population who walk for at least 30 minutes, at least 5 x week",
+                        Attribute.DataType.numeric
+               );
+            case cycle1xWeek:
+                return new Attribute(
+                        getProvider(),
+                        AttributeId.cycle1xWeek.name(),
+                        "Cycle 1x Week",
+                        "% of population who cycle at least 1 x week",
+                        Attribute.DataType.numeric
+                );
+            default:
+                return null;
+        }
+    }
+}
---
+++ src/test/java/uk/org/tombolo/importer/londondatastore/WalkingCyclingBoroughImporterTest.java
@@ -0,0 +1,79 @@
+package uk.org.tombolo.importer.londondatastore;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.org.tombolo.AbstractTest;
+import uk.org.tombolo.TestFactory;
+import uk.org.tombolo.core.Attribute;
+import uk.org.tombolo.core.Datasource;
+import uk.org.tombolo.core.Subject;
+import uk.org.tombolo.core.TimedValue;
+import uk.org.tombolo.core.utils.AttributeUtils;
+import uk.org.tombolo.core.utils.TimedValueUtils;
+import uk.org.tombolo.importer.Importer;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class WalkingCyclingBoroughImporterTest extends AbstractTest {
+	private static final String DATASOURCE_ID = "walkingCyclingBorough";
+	public Importer importer;
+
+	Subject cityOfLondon;
+
+	@Before
+	public void addSubjectFixtures() {
+		cityOfLondon = TestFactory.makeNamedSubject("E09000001");
+	}
+
+	@Before
+	public void before(){
+		importer = new WalkingCyclingBoroughImporter();
+		mockDownloadUtils(importer);
+	};
+
+	@Test
+	public void testGetAllDatasources() throws Exception {
+		List<Datasource> datasources = importer.getAllDatasources();
+		assertEquals(1, datasources.size());
+	}
+
+	@Test
+	public void testGetDatasource() throws Exception {
+		Datasource datasource = importer.getDatasource(DATASOURCE_ID);
+		
+		List<Attribute> attributes = datasource.getAttributes();
+		assertEquals(2, attributes.size());
+	}
+	
+	@Test
+	public void testImportDatasource() throws Exception{
+		int datapoints = importer.importDatasource(DATASOURCE_ID);
+		
+		assertEquals(8, datapoints);
+
+		LocalDateTime year2012 = LocalDateTime.parse("2012-12-31T23:59:59");
+		LocalDateTime year2014 = LocalDateTime.parse("2014-12-31T23:59:59");
+
+		List<TimedValue> walk5xWeek = TimedValueUtils.getBySubjectAndAttribute(
+				cityOfLondon,
+				AttributeUtils.getByProviderAndLabel(importer.getProvider(), "walk5xWeek"));
+		assertEquals(4, walk5xWeek.size());
+		TimedValue walk5xWeek2012 = walk5xWeek.stream().filter(a -> a.getId().getTimestamp().equals(year2012)).findFirst().orElse(null);
+		assertEquals(31d, walk5xWeek2012.getValue(), 0.1d);
+		TimedValue walk5xWeek2014 = walk5xWeek.stream().filter(a -> a.getId().getTimestamp().equals(year2014)).findFirst().orElse(null);
+		assertEquals(57d, walk5xWeek2014.getValue(), 0.1d);
+
+		List<TimedValue> cycle1xWeek = TimedValueUtils.getBySubjectAndAttribute(
+				cityOfLondon,
+				AttributeUtils.getByProviderAndLabel(importer.getProvider(), "cycle1xWeek"));
+		assertEquals(4, cycle1xWeek.size());
+		TimedValue cycle1xWeek2012 = cycle1xWeek.stream().filter(a -> a.getId().getTimestamp().equals(year2012)).findFirst().orElse(null);
+		assertEquals(16d, cycle1xWeek2012.getValue(), 0.1d);
+		TimedValue cycle1xWeek2014 = cycle1xWeek.stream().filter(a -> a.getId().getTimestamp().equals(year2014)).findFirst().orElse(null);
+		assertEquals(7d, cycle1xWeek2014.getValue(), 0.1d);
+	}
+}
