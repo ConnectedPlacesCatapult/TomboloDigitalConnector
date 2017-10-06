37cff5d18e69b5438a95338ce5f6af946ee76e5a
Borkur Sigurbjornsson <bsigurbjornsson@futurecities.catapult.org.uk>
2017-10-06 12:22:59 +0100

    Updating London Datastore importes while updating data for OrganiCity

---
+++ src/test/java/uk/org/tombolo/importer/londondatastore/AbstractLondonDatastoreTestUtil.java
@@ -0,0 +1,19 @@
+package uk.org.tombolo.importer.londondatastore;
+
+import org.junit.Before;
+import uk.org.tombolo.AbstractTest;
+import uk.org.tombolo.TestFactory;
+import uk.org.tombolo.core.Subject;
+
+public abstract class AbstractLondonDatastoreTestUtil extends AbstractTest {
+
+    Subject cityOfLondon;
+    Subject islington;
+
+    @Before
+    public void addSubjectFixtures() {
+        cityOfLondon = TestFactory.makeNamedSubject("E09000001");
+        islington = TestFactory.makeNamedSubject("E09000019");
+    }
+
+}
