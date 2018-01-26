5cf16f95d6de70e8a6888229d3da2f2682f6e357
Hemanshu <arya.hemanshu@gmail.com>
2018-01-26 16:37:52 +0000

    added spatial joins to json schema file. Reverted trafficCountImporter to be inherited from AbstractImporter

--- src/main/java/uk/org/tombolo/importer/dft/AbstractDFTImporter.java
+++ src/main/java/uk/org/tombolo/importer/dft/AbstractDFTImporter.java
@@ -1,10 +1,11 @@
 package uk.org.tombolo.importer.dft;
 
 import uk.org.tombolo.core.Provider;
+import uk.org.tombolo.importer.AbstractImporter;
 import uk.org.tombolo.importer.AbstractOaImporter;
 import uk.org.tombolo.importer.Config;
 
-public abstract class AbstractDFTImporter extends AbstractOaImporter {
+public abstract class AbstractDFTImporter extends AbstractImporter {
     public static final Provider PROVIDER = new Provider(
             "uk.gov.dft",
             "Department for Transport"
