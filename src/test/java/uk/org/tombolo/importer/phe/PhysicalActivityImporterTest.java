e11c836a91f45fab79d0659f673ddfd9c02d824f
thanosbnt <TBantis@futurecities.catapult.org.uk>
2018-02-22 14:50:33 +0000

    added MVPA datasource

--- src/test/java/uk/org/tombolo/importer/phe/PhysicalActivityImporterTest.java
+++
@@ -1,9 +0,0 @@
-package uk.org.tombolo.importer.phe;
-
-import uk.org.tombolo.AbstractTest;
-
-/**
- * Created by tbantis on 21/02/2018.
- */
-public class PhysicalActivityImporterTest extends AbstractTest {
-}
