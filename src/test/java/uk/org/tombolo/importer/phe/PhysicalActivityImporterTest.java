71b4ee0ebe9c38e442345d5388af3df0785feb2c
thanosbnt <TBantis@futurecities.catapult.org.uk>
2018-02-22 09:42:57 +0000

    in progress

---
+++ src/test/java/uk/org/tombolo/importer/phe/PhysicalActivityImporterTest.java
@@ -0,0 +1,9 @@
+package uk.org.tombolo.importer.phe;
+
+import uk.org.tombolo.AbstractTest;
+
+/**
+ * Created by tbantis on 21/02/2018.
+ */
+public class PhysicalActivityImporterTest extends AbstractTest {
+}
