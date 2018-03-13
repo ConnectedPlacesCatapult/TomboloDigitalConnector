67eb0d83340a117d11e7490fc36f75e0ba273932
Hemanshu <arya.hemanshu@gmail.com>
2018-03-13 16:46:35 +0000

    added functionality for user to get information about importers through terminal

---
+++ src/test/java/uk/org/tombolo/ImporterSpecificationRunnerTest.java
@@ -0,0 +1,64 @@
+package uk.org.tombolo;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import uk.org.tombolo.core.Datasource;
+import uk.org.tombolo.importer.Importer;
+import uk.org.tombolo.importer.dft.AccessibilityImporter;
+import uk.org.tombolo.importer.nhschoices.HealthOrganisationImporter;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+public class ImporterSpecificationRunnerTest {
+
+    Importer importer;
+    Datasource datasource;
+    ImporterSpecificationRunner importerSpecificationRunner = new ImporterSpecificationRunner();;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        importer = importerSpecificationRunner.getImporter("uk.org.tombolo.importer.dft.TrafficCountImporter");
+        datasource = importerSpecificationRunner.getDatasource("trafficVolume", importer);
+
+    }
+
+    @Test
+    public void testGetDatasourceIds() throws Exception {
+        List<String> datasources = importerSpecificationRunner.getDatasourceIds(importer);
+        assertEquals(2, datasources.size());
+    }
+
+    @Test
+    public void testGetDatasource() throws Exception {
+        assertEquals("trafficCounter", datasource.getSubjectTypes().get(0).getLabel());
+    }
+
+    @Test
+    public void testGetTimedValueAttributes() throws Exception {
+        List<String> timedValues = importerSpecificationRunner.getTimedValueAttributes(datasource);
+        assertEquals(6, timedValues.size());
+    }
+
+    @Test
+    public void testGetFixedValueAttributes() throws Exception {
+        List<String> fixedValues = importerSpecificationRunner.getFixedValueAttributes(datasource);
+        assertEquals(4, fixedValues.size());
+    }
+
+    @Test
+    public void testGetSubjectType() throws Exception {
+        List<String> subjectTypes = importerSpecificationRunner.getSubjectType(datasource);
+        assertEquals(1, subjectTypes.size());
+    }
+
+    @Test
+    public void testGetProvider() throws Exception {
+        assertEquals("uk.gov.dft", importer.getProvider().getLabel());
+    }
+}
